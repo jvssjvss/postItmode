07dce4acf81c8aff67791c068f740cc486cce149
João Victor Sousa Santos <52776398+jvssjvss@users.noreply.github.com>
2021-05-19 01:56:49 +0200

    Texto dinámico.
    Añadida la funcionalidade de escribir el texto el post-it dentro el documento html.

--- postIt.js
+++ postIt.js
@@ -1,51 +1,59 @@
 
+//marcar o número de clicks
 window.click=0;
+
+//Trocar as cores do post-it
 function postItColor(){
-    const colorPost= ['lightgreen', 'pink', 'lightblue', 'lightyellow'];
+    const colorPost= ['lightgreen', 'pink', 'lightblue', '#FFFF66'];
     let colorIndex = Math.floor(Math.random()*colorPost.length);
     let actualColor = colorPost[colorIndex];
     return (note.style.backgroundColor = actualColor);
-
 }
 
+//Tornar visível o post-it
 function postIt(){
     window.note=document.getElementById("postar");
     note.style.display="block";
+    postItColor();
 
 }
 
+//Apagar o post-it e zerar o click
 function erasePost(){
-    note.style.display="none"}
+    note.style.display="none";
+    click=0;}
 
 function postContent(content){
     note.innerText = content;
 }
-
-function dinamicPost(secuencia){
-    let postItems=["0", "joao", "2", "3"];
-    return postItems[secuencia];
-    
+function dinamicPost(indicador, item){
+let matrix = [];
+matrix.length=indicador;
+for (i=0; i<matrix.length; i++){
+    matrix[i]= document.getElementById(nomeSpan+i).innerText;
+    console.log(i)
+   } return matrix[item];
 }
+
+
+
 
 function onPost(){
     for (i=0; i<4; i++){
     setTimeout(()=> postContent(dinamicPost(click)), i*1000);}}
-    
 
-function clickPost(){
-   
+function clickPost(itemFinal){
     click=click+1;
     postItColor();
 
+
     switch(click){
-        case 4:
+        case itemFinal:
             click=0;
-            postContent(dinamicPost(click));
+            postContent(dinamicPost(itemFinal, click));
             break;
         default:
-            postContent(dinamicPost(click));
+            postContent(dinamicPost(itemFinal, click));
             break;
-
     }
-   
 }
